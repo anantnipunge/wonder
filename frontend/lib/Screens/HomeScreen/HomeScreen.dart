074535d0344a44e0e558526ec6cd84cb27dee599
akash <kamatakash13@gmail.com>
2023-10-03 23:35:19 +0530

    new: PopularCarousel created

--- frontend/lib/Screens/HomeScreen/HomeScreen.dart
+++ frontend/lib/Screens/HomeScreen/HomeScreen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/Models/Product.dart';
 import 'package:frontend/Screens/HomeScreen/components/Categories.dart';
 import 'package:frontend/Screens/HomeScreen/components/MySearchBar.dart';
+import 'package:frontend/Screens/HomeScreen/components/PopularCarousel.dart';
+// import 'package:frontend/assets';
 
 import '../../constants/constants.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  //const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeState();
@@ -15,14 +19,18 @@ class _HomeState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // resizeToAvoidBottomInset: false,
       appBar: buildAppBar(),
-      body: const SafeArea(
+      body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(16.0),
           child: Column(
             children: [
               MySearchBar(),
               Categories(),
+              PopularCarousel(
+                products: productList,
+              ),
             ],
           ),
         ),
